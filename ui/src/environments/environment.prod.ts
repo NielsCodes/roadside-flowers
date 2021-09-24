539409c3f3cb910aaa623ca090622b854e3e1a14
Niels Kersic <hi@niels.codes>
2021-09-24 21:22:33 +0200

    Revert prod API endpoint
    Revert to default Cloud Run URL while custom domain has not yet been provisioned by GCP

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   /** API endpoint for authentication purposes */
-  endpoint: 'https://roadsideflowers.apis.niels.codes',
+  // endpoint: 'https://roadsideflowers.apis.niels.codes',
+  endpoint: 'https://roadside-flowers-api-prod-uhv4eba3aa-uc.a.run.app',
   /** Redirect URL for Spotify authentication */
   redirect: 'https://roadsideflowers.niels.codes/callback'
 };
