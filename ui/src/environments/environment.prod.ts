d12619b6e42ac7284462871ac5e1a6e4ac1602d8
Niels Kersic <hi@niels.codes>
2021-09-25 20:26:03 +0200

    Update prod API endpoint
    Update environment to use named API endpoint instead of default Cloud Run URL

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   /** API endpoint for authentication purposes */
-  // endpoint: 'https://roadsideflowers.apis.niels.codes',
-  endpoint: 'https://roadside-flowers-api-prod-uhv4eba3aa-uc.a.run.app',
+  endpoint: 'https://roadsideflowers.apis.niels.codes',
   /** Redirect URL for Spotify authentication */
   redirect: 'https://roadsideflowers.niels.codes/callback'
 };
